41b00a1809a7658aa7782b14dcd7ce6fb31d38db
Hiick <alex.mignon77@gmail.com>
2018-12-14 12:57:24 +0100

    prod

---
+++ conf/production.js
@@ -0,0 +1,14 @@
+module.exports = {
+	mono: {
+		jwt: {
+			secret: '$2a$15$3tCF3cqOYsZurRGcE0ND3e3HzoPxBEkbrBkQPL9JKxsgvC7jh6R7.',
+		},
+		http: {
+			logLevel: 'combined'
+		},
+		mongodb: {
+			url: 'cluster0-bzfup.gcp.mongodb.net:27017',
+			dbName: 'foxys'
+		}
+	}
+}
